7da4befe333ed224bae5d538bd0e79f81a1c5846
zzzam21 <jonas01311@gmail.com>
2026-04-13 20:58:16 -0500

    feat: adding restFramework

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #RestFramework
+    'rest_framework',
     #local
     "tournaments.apps.TournamentsConfig",
+    
 ]
 
 MIDDLEWARE = [
